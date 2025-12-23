2e89cbc92664663c5174e436aeedae51769c6e3a
saizhuolin001 <saizhuolin001@ke.com>
2025-12-23 16:46:36 +0800

    feat: update ico

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Debug Life',
   tagline: '用程序员的视角解构生活，寻找人生最优解',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
