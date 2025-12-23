8e17a6293c979cb0b8f7e3c96dd140aede1fd1cf
saizhuolin001 <saizhuolin001@ke.com>
2025-12-23 16:44:47 +0800

    feat: mobile device css

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: ['./src/css/custom.css', './src/css/navbar-fix.css'],
         },
       } satisfies Preset.Options,
     ],
